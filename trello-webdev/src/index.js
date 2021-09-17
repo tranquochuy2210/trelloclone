38c23dc807de5bf0545c1ac7c5c26c9b909d733d
tranhuy <tranquochuy221096@gmail.com>
2021-09-17 12:12:06 +0700

    handle_drag_drop

--- trello-webdev/src/index.js
+++ trello-webdev/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'font-awesome/css/font-awesome.min.css'
+import App from './App'
 
 
 ReactDOM.render(
